bb605114803b28806ea36fd02cb023292aa1a98c
maxas <vpodkopaev@knwlab.com>
2021-01-03 11:18:33 +0300

    navigation was updated

--- app/src/main/java/com/bunbeauty/papakarlo/ui/main/MainFragment.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/ui/main/MainFragment.kt
@@ -2,20 +2,15 @@ package com.bunbeauty.papakarlo.ui.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.bunbeauty.papakarlo.BR
 import com.bunbeauty.papakarlo.R
 import com.bunbeauty.papakarlo.databinding.FragmentMainBinding
 import com.bunbeauty.papakarlo.di.components.ViewModelComponent
-import com.bunbeauty.papakarlo.generated.callback.OnClickListener
+import com.bunbeauty.papakarlo.extensions.setupWithNavController
 import com.bunbeauty.papakarlo.ui.base.CartClickableFragment
-import com.bunbeauty.papakarlo.ui.base.TopBarFragment
 import com.bunbeauty.papakarlo.ui.main.MainFragmentDirections.actionMainFragmentToCartFragment
 import com.bunbeauty.papakarlo.view_model.MainViewModel
-import kotlin.reflect.KFunction
-import kotlin.reflect.KFunction1
 
 class MainFragment: CartClickableFragment<FragmentMainBinding, MainViewModel>(), MainNavigator {
 
@@ -27,12 +22,36 @@ class MainFragment: CartClickableFragment<FragmentMainBinding, MainViewModel>(),
         viewModelComponent.inject(this)
     }
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        setupBottomNavigation()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        NavigationUI.setupWithNavController(
+        /*NavigationUI.setupWithNavController(
             viewDataBinding.fragmentMainBnvNavigationBar,
             (childFragmentManager.findFragmentById(R.id.fragment_main_fcv_container) as NavHostFragment).findNavController()
+        )*/
+        if (savedInstanceState == null) {
+            setupBottomNavigation()
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        val navGraphIdList = listOf(
+            R.navigation.nav_menu,
+            R.navigation.nav_orders,
+            R.navigation.nav_contacts
+        )
+
+        viewDataBinding.fragmentMainBnvNavigationBar.setupWithNavController(
+            navGraphIdList,
+            childFragmentManager,
+            R.id.fragment_main_fcv_container,
+            requireActivity().intent
         )
     }
 
