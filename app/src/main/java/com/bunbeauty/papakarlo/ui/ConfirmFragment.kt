ef1d6f251a418c307f127ad5f68c21653320e0fc
xIdealo <shavl.mark@yandex.ru>
2021-04-15 18:27:34 +0300

    added ConfirmFragment.kt up desing on fragment_login.xml

---
+++ app/src/main/java/com/bunbeauty/papakarlo/ui/ConfirmFragment.kt
@@ -0,0 +1,13 @@
+package com.bunbeauty.papakarlo.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bunbeauty.papakarlo.R
+
+
+class ConfirmFragment : Fragment() {
+
+}
