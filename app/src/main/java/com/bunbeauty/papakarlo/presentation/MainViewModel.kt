3fb9a18b21e75b9b4155deba615cd6d05ab6905b
maxastin <max.astin@yandex.ru>
2021-12-19 18:50:32 +0300

    update:gitignore

--- app/src/main/java/com/bunbeauty/papakarlo/presentation/MainViewModel.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/presentation/MainViewModel.kt
@@ -1,16 +1,7 @@
 package com.bunbeauty.papakarlo.presentation
 
-import androidx.lifecycle.viewModelScope
-import com.bunbeauty.common.Logger.TEST_TAG
-import com.bunbeauty.common.Logger.logD
 import com.bunbeauty.domain.interactor.main.IMainInteractor
 import com.bunbeauty.papakarlo.presentation.base.BaseViewModel
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.features.websocket.*
-import io.ktor.http.*
-import io.ktor.http.cio.websocket.*
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor() : BaseViewModel() {
@@ -20,33 +11,33 @@ class MainViewModel @Inject constructor() : BaseViewModel() {
         mainInteractor.refreshData()
     }
 
-    fun connectWS() {
-        val client = HttpClient(OkHttp) {
-            install(WebSockets)
-        }
-
-        viewModelScope.launch {
-            logD(TEST_TAG, "connectWS")
-            try {
-                client.webSocket(
-                    HttpMethod.Get,
-                    host = "food-delivery-api-bunbeauty.herokuapp.com",
-                    port = 8080,
-                    path = "/topic/orders"
-                ) {
-                    logD(TEST_TAG, "webSocket /topic/orders")
-                    while (true) {
-                        val othersMessage = incoming.receive() as? Frame.Text ?: continue
-                        logD(TEST_TAG, "/topic/orders " + othersMessage.readText())
-                        val myMessage = readLine()
-                        if (myMessage != null) {
-                            send(myMessage)
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                logD(TEST_TAG, "ex " + e.message)
-            }
-        }
-    }
+//    fun connectWS() {
+//        val client = HttpClient(OkHttp) {
+//            install(WebSockets)
+//        }
+//
+//        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhdXRoIiwiaXNzIjoiRm9vZERlbGl2ZXJ5QXBpIiwidXNlclV1aWQiOiIwY2FjNzBkOC04ZDg1LTRjNTQtOWYwNS03NWI2M2U3ZjUyNjYiLCJ1c2VyUm9sZSI6Im1hbmFnZXIifQ.FmAGUaAWVXQ1pnrwG_58PmvGmLZpWe_GXMkCcNf53ks"
+//        viewModelScope.launch {
+//            logD(TEST_TAG, "connectWS")
+//            try {
+//                client.webSocket(
+//                    HttpMethod.Get,
+//                    host = "food-delivery-api-bunbeauty.herokuapp.com",
+//                    path = "user/order/subscribe",
+//                    request = {
+//                        header("Authorization", "Bearer $token")
+//                        parameter("cafeUuid", "2efe19e0-4324-4c5f-b214-c5cc6da2dacf")
+//                    }
+//                ) {
+//                    logD(TEST_TAG, "webSocket /order/subscribe")
+//                    while (true) {
+//                        val othersMessage = incoming.receive() as? Frame.Text ?: continue
+//                        logD(TEST_TAG, "income " + othersMessage.readText())
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                logD(TEST_TAG, "ex " + e.message)
+//            }
+//        }
+//    }
 }
