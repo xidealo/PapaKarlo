50d7520d22cf8dbf8434b201401fff5ce49ed662
xidealo <shavl.mark@yandex.ru>
2022-03-09 12:54:28 +0300

    fix String Ext

--- PapaKarloSwift/PapaKarloSwift/feature/product_details/ProductDetailsView.swift
+++ PapaKarloSwift/PapaKarloSwift/feature/product_details/ProductDetailsView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+extension String {
+    
+    func load() -> UIImage {
+        do {
+            guard let url = URL(string: self) else {
+                return UIImage()
+            }
+            let data:Data = try Data(contentsOf: url)
+            return UIImage(data: data) ?? UIImage()
+        }catch {
+            return UIImage()
+        }
+    }
+}
+
 struct ProductDetailsView: View {
     let menuProductUI:MenuProductUI
     var body: some View {
