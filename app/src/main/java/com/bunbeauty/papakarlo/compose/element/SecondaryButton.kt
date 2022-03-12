6c6b9274cfc2b38c6ee0f8bfce74b85bec72ca76
maxastin <max.astin@yandex.ru>
2022-03-12 13:45:17 +0300

    refactoring:buttons

--- app/src/main/java/com/bunbeauty/papakarlo/compose/element/SecondaryButton.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/compose/element/SecondaryButton.kt
@@ -1,12 +1,18 @@
 package com.bunbeauty.papakarlo.compose.element
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bunbeauty.papakarlo.R
@@ -20,20 +26,40 @@ fun SecondaryButton(
     hasShadow: Boolean = true,
     onClick: () -> Unit,
 ) {
-    Button(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(FoodDeliveryTheme.dimensions.buttonSize),
-        colors = FoodDeliveryTheme.colors.secondaryButtonColors(),
-        shape = mediumRoundedCornerShape,
-        elevation = FoodDeliveryTheme.dimensions.getButtonEvaluation(hasShadow),
-        onClick = onClick,
-    ) {
-        Text(
-            text = stringResource(textStringId).uppercase(),
-            style = FoodDeliveryTheme.typography.button,
-            color = FoodDeliveryTheme.colors.onSecondary
-        )
+    Box(modifier = modifier.height(IntrinsicSize.Min)) {
+        Card(
+            modifier = Modifier
+                .defaultMinSize(minHeight = FoodDeliveryTheme.dimensions.buttonSize)
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .shadow(
+                    elevation = FoodDeliveryTheme.dimensions.getEvaluation(hasShadow),
+                    shape = mediumRoundedCornerShape
+                )
+                .clip(mediumRoundedCornerShape)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(),
+                    onClick = onClick
+                ),
+            backgroundColor = FoodDeliveryTheme.colors.secondary
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        horizontal = FoodDeliveryTheme.dimensions.mediumSpace,
+                        vertical = FoodDeliveryTheme.dimensions.smallSpace
+                    )
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = stringResource(textStringId).uppercase(),
+                    style = FoodDeliveryTheme.typography.button,
+                    color = FoodDeliveryTheme.colors.onSecondary
+                )
+            }
+        }
     }
 }
 
