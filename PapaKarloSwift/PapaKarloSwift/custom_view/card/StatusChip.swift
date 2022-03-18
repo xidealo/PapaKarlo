0dc1a1be3f75ab2b7b174d7f390e53d42b5a6d34
xidealo <shavl.mark@yandex.ru>
2022-03-18 21:22:44 +0300

    add login

---
+++ PapaKarloSwift/PapaKarloSwift/custom_view/card/StatusChip.swift
@@ -0,0 +1,30 @@
+//
+//  StatusChip.swift
+//  PapaKarloSwift
+//
+//  Created by Марк Шавловский on 17.03.2022.
+//
+
+import SwiftUI
+
+struct StatusChip: View {
+    
+    let status:String
+    
+    var body: some View {
+        Text(status)
+            .foregroundColor(Color("surface"))
+            .frame(height:30)
+            .padding(Diems.SMALL_PADDING)
+            .background(Color.blue)//select color
+            .font(.system(size: Diems.MEDIUM_TEXT_SIZE, weight: .heavy, design: .default))
+            .cornerRadius(Diems.MEDIUM_RADIUS)
+        
+    }
+}
+
+struct StatusChip_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusChip(status: "PREPATING")
+    }
+}
---
+++ PapaKarloSwift/PapaKarloSwift/feature/auth/confirm/ConfirmView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfirmView.swift
+//  PapaKarloSwift
+//
+//  Created by Марк Шавловский on 18.03.2022.
+//
+
+import SwiftUI
+
+struct ConfirmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfirmView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmView()
+    }
+}
