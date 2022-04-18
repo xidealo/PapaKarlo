2561e1a9510d36b6eb02f210cb0e656efd307865
maxastin <max.astin@yandex.ru>
2022-04-18 16:56:55 +0300

    remove: extra workers

--- app/src/main/java/com/bunbeauty/papakarlo/di/modules/WorkerModule.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/di/modules/WorkerModule.kt
@@ -5,7 +5,6 @@ import com.bunbeauty.papakarlo.worker.cafe.CafeWorkerUtil
 import com.bunbeauty.papakarlo.worker.city.CityWorkerUtil
 import com.bunbeauty.papakarlo.worker.delivery.DeliveryWorkerUtil
 import com.bunbeauty.papakarlo.worker.street.StreetWorkerUtil
-import com.bunbeauty.papakarlo.worker.user.UserWorkerUtil
 import org.koin.dsl.module
 
 fun workerModule() = module {
@@ -13,5 +12,4 @@ fun workerModule() = module {
     single<IStreetWorkerUtil> { StreetWorkerUtil(workManager = get()) }
     single<ICityWorkerUtil> { CityWorkerUtil(workManager = get()) }
     single<IDeliveryWorkerUtil> { DeliveryWorkerUtil(workManager = get()) }
-    single<IUserWorkerUtil> { UserWorkerUtil(workManager = get()) }
 }
