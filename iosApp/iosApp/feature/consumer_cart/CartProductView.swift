7e8743cbe3ff1d05d33c83aba58285cd485cd6a0
xidealo <shavl.mark@yandex.ru>
2022-09-03 17:36:17 +0300

    fix count picker fix Menu with LargeHeaderText.swift

--- iosApp/iosApp/feature/consumer_cart/CartProductView.swift
+++ iosApp/iosApp/feature/consumer_cart/CartProductView.swift
@@ -30,8 +30,9 @@ struct CartProductView: View {
                     if cartProductItem.oldCost != nil {
                         StrikeText(text: String(cartProductItem.oldCost!))
                     }
-                    Text(cartProductItem.newCost).frame(maxWidth:.infinity, alignment: .topLeading)                    .foregroundColor(Color("onSurface"))
-
+                    Text(cartProductItem.newCost)
+                        .frame(maxWidth:.infinity, alignment: .topLeading)
+                        .foregroundColor(Color("onSurface"))
                 }
             }.frame(maxHeight: Diems.IMAGE_ELEMENT_HEIGHT)
             
