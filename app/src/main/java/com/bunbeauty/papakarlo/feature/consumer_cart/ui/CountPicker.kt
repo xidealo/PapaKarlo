e7282db7bcfc8765a37a93c590c35b0c6ac7134a
xIdealo <shavl.mark@yandex.ru>
2023-03-11 01:31:51 +0300

    fix CategoryItem.kt

--- app/src/main/java/com/bunbeauty/papakarlo/feature/consumer_cart/ui/CountPicker.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/feature/consumer_cart/ui/CountPicker.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -34,7 +35,9 @@ fun CountPicker(
 ) {
     Row(
         modifier = modifier
-            .wrapContentHeight()
+            .heightIn(
+                min = FoodDeliveryTheme.dimensions.smallButtonSize
+            )
             .clip(buttonRoundedCornerShape)
             .border(
                 BorderStroke(2.dp, FoodDeliveryTheme.colors.primary),
