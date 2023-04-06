dc88f1769c70b78b79dcf39dde519ae9888535d2
v.podkopaev <max.astin@yandex.ru>
2023-04-07 00:28:46 +0300

    update company uuid for ios

--- shared/src/iosMain/kotlin/com/bunbeauty/shared/data/CompanyUuid.kt
+++ shared/src/iosMain/kotlin/com/bunbeauty/shared/data/CompanyUuid.kt
@@ -1,7 +1,19 @@
 package com.bunbeauty.shared.data
 
+import com.bunbeauty.shared.Constants.PAPA_KARLO_COMPANY_UUID
+import com.bunbeauty.shared.Constants.PAPA_KARLO_FLAVOR_NAME
+import com.bunbeauty.shared.Constants.TEST_COMPANY_UUID
+import com.bunbeauty.shared.Constants.YULIAR_COMPANY_UUID
+import com.bunbeauty.shared.Constants.YULIAR_FLAVOR_NAME
+import com.bunbeauty.shared.domain.exeptions.UnknownFlavorException
+import platform.Foundation.NSBundle
+
 internal actual val companyUuid: String = if (Platform.isDebugBinary) {
-    "fd483dcb-3f44-457f-b4d4-f82d2aa83b46"
+    TEST_COMPANY_UUID
 } else {
-    "7416dba5-2825-4fe3-abfb-1494a5e2bf99"
+    when (NSBundle.mainBundle.bundleIdentifier?.split('.')?.lastOrNull()) {
+        YULIAR_FLAVOR_NAME.lowercase() -> YULIAR_COMPANY_UUID
+        PAPA_KARLO_FLAVOR_NAME.lowercase() -> PAPA_KARLO_COMPANY_UUID
+        else -> throw UnknownFlavorException()
+    }
 }
