be7aa162f940b576118ce44c94d9d56a18863843
v.podkopaev <max.astin@yandex.ru>
2023-04-08 21:24:30 +0300

    update target check

--- shared/src/commonMain/kotlin/com/bunbeauty/shared/Constants.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/Constants.kt
@@ -5,9 +5,11 @@ object Constants {
     // COMPANY
     const val TEST_COMPANY_UUID = "fd483dcb-3f44-457f-b4d4-f82d2aa83b46"
 
+    const val PAPA_KARLO_TARGET_NAME = "papakarlo"
     const val PAPA_KARLO_FLAVOR_NAME = "papaKarlo"
     const val PAPA_KARLO_COMPANY_UUID = "7416dba5-2825-4fe3-abfb-1494a5e2bf99"
 
+    const val YULIAR_TARGET_NAME = "yuliar"
     const val YULIAR_FLAVOR_NAME = "yuliar"
     const val YULIAR_COMPANY_UUID = "8b91126f-be08-423a-b1dc-dea78ae79cd0"
 
--- shared/src/iosMain/kotlin/com/bunbeauty/shared/data/CompanyUuid.kt
+++ shared/src/iosMain/kotlin/com/bunbeauty/shared/data/CompanyUuid.kt
@@ -1,19 +1,18 @@
 package com.bunbeauty.shared.data
 
 import com.bunbeauty.shared.Constants.PAPA_KARLO_COMPANY_UUID
-import com.bunbeauty.shared.Constants.PAPA_KARLO_FLAVOR_NAME
+import com.bunbeauty.shared.Constants.YULIAR_TARGET_NAME
 import com.bunbeauty.shared.Constants.TEST_COMPANY_UUID
 import com.bunbeauty.shared.Constants.YULIAR_COMPANY_UUID
-import com.bunbeauty.shared.Constants.YULIAR_FLAVOR_NAME
+import com.bunbeauty.shared.Constants.PAPA_KARLO_TARGET_NAME
 import com.bunbeauty.shared.domain.exeptions.UnknownFlavorException
-import platform.Foundation.NSBundle
 
 internal actual val companyUuid: String = if (Platform.isDebugBinary) {
     TEST_COMPANY_UUID
 } else {
-    when (NSBundle.mainBundle.bundleIdentifier?.split('.')?.lastOrNull()) {
-        YULIAR_FLAVOR_NAME.lowercase() -> YULIAR_COMPANY_UUID
-        PAPA_KARLO_FLAVOR_NAME.lowercase() -> PAPA_KARLO_COMPANY_UUID
+    when (targetName) {
+        YULIAR_TARGET_NAME -> YULIAR_COMPANY_UUID
+        PAPA_KARLO_TARGET_NAME -> PAPA_KARLO_COMPANY_UUID
         else -> throw UnknownFlavorException()
     }
 }
