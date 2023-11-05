c8fb365b8342de78e3f52903ad08889ea17ec307
xIdealo <shavl.mark@yandex.ru>
2023-11-05 22:34:02 +0300

    update build.gradle.kts

--- analytic/build.gradle.kts
+++ analytic/build.gradle.kts
@@ -51,4 +51,8 @@ android {
     defaultConfig {
         minSdk = AndroidSdk.min
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
