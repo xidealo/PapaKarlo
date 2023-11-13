696deedbdf8df222779f3e45ceda08b8d17b107a
v.podkopaev <max.astin@yandex.ru>
2023-11-13 22:33:14 +0200

    fix color changing after auto scroll

--- app/src/main/java/com/bunbeauty/papakarlo/common/ui/element/FoodDeliveryScaffold.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/common/ui/element/FoodDeliveryScaffold.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import com.bunbeauty.papakarlo.common.ui.element.topbar.FoodDeliveryTopAppBar
 import com.bunbeauty.papakarlo.common.ui.element.topbar.FoodDeliveryToolbarActions
 import com.bunbeauty.papakarlo.common.ui.element.topbar.FoodDeliveryTopAppBar
 import com.bunbeauty.papakarlo.common.ui.theme.FoodDeliveryTheme
