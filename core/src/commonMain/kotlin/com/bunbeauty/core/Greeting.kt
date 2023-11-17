113e2a44e016e8d16f6dc2988edbeeaf9fe60e35
xidealo <shavl.mark@yandex.ru>
2023-11-17 23:42:05 +0300

    update core module

--- core/src/commonMain/kotlin/com/bunbeauty/core/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.bunbeauty.core
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- core/src/commonMain/kotlin/com/bunbeauty/core/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.bunbeauty.core
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
