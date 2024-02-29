9bcf7bec68d24844cceeaef044bc5f40cfa4d4a8
v.podkopaev <max.astin@yandex.ru>
2024-02-29 23:50:44 +0300

    add orders limit

--- shared/src/commonMain/kotlin/com/bunbeauty/shared/data/dao/order/IOrderDao.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/data/dao/order/IOrderDao.kt
@@ -2,21 +2,14 @@ package com.bunbeauty.shared.data.dao.order
 
 import com.bunbeauty.shared.db.OrderEntity
 import com.bunbeauty.shared.db.OrderWithProductEntity
-import kotlinx.coroutines.flow.Flow
 
 interface IOrderDao {
 
     suspend fun insertOrder(orderEntity: OrderEntity)
 
-    suspend fun getLastOrderByUserUuid(userUuid: String): OrderEntity?
-    suspend fun getOrderListByUserUuid(userUuid: String): List<OrderEntity>
+    suspend fun getOrderListByUserUuid(userUuid: String, count: Int): List<OrderEntity>
     suspend fun getOrderWithProductListByUserUuid(userUuid: String): List<OrderWithProductEntity>
     suspend fun getOrderWithProductListByUuid(uuid: String): List<OrderWithProductEntity>
-
-    fun observeOrderWithProductListByUserUuid(userUuid: String): Flow<List<OrderWithProductEntity>>
-    fun observeOrderWithProductListByUuid(uuid: String): Flow<List<OrderWithProductEntity>>
-    fun observeOrderListByUserUuid(userUuid: String): Flow<List<OrderEntity>>
-    fun observeLastOrderByUserUuid(userUuid: String): Flow<OrderEntity?>
 
     suspend fun updateOrderStatusByUuid(uuid: String, status: String)
 }
--- shared/src/commonMain/kotlin/com/bunbeauty/shared/data/dao/order/OrderDao.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/data/dao/order/OrderDao.kt
@@ -3,10 +3,6 @@ package com.bunbeauty.shared.data.dao.order
 import com.bunbeauty.shared.db.FoodDeliveryDatabase
 import com.bunbeauty.shared.db.OrderEntity
 import com.bunbeauty.shared.db.OrderWithProductEntity
-import com.squareup.sqldelight.runtime.coroutines.asFlow
-import com.squareup.sqldelight.runtime.coroutines.mapToList
-import com.squareup.sqldelight.runtime.coroutines.mapToOneOrNull
-import kotlinx.coroutines.flow.Flow
 
 class OrderDao(foodDeliveryDatabase: FoodDeliveryDatabase) : IOrderDao {
 
@@ -16,35 +12,15 @@ class OrderDao(foodDeliveryDatabase: FoodDeliveryDatabase) : IOrderDao {
         orderEntityQueries.isnsertOrder(orderEntity)
     }
 
-    override suspend fun getLastOrderByUserUuid(userUuid: String): OrderEntity? {
-        return orderEntityQueries
-            .getLastOrderWithProductListByUserUuid(userUuid)
-            .executeAsOneOrNull()
-    }
-
-    override suspend fun getOrderListByUserUuid(userUuid: String): List<OrderEntity> {
-        return orderEntityQueries.getOrderListByUserUuid(userUuid).executeAsList()
+    override suspend fun getOrderListByUserUuid(
+        userUuid: String,
+        count: Int
+    ): List<OrderEntity> {
+        return orderEntityQueries.getOrderListByUserUuid(userUuid, count.toLong()).executeAsList()
     }
 
     override suspend fun getOrderWithProductListByUserUuid(userUuid: String): List<OrderWithProductEntity> {
         return orderEntityQueries.getOrderWithProductListByUserUuid(userUuid).executeAsList()
-    }
-
-    override fun observeOrderWithProductListByUserUuid(userUuid: String): Flow<List<OrderWithProductEntity>> {
-        return orderEntityQueries.getOrderWithProductListByUserUuid(userUuid).asFlow().mapToList()
-    }
-
-    override fun observeOrderListByUserUuid(userUuid: String): Flow<List<OrderEntity>> {
-        return orderEntityQueries.getOrderListByUserUuid(userUuid).asFlow().mapToList()
-    }
-
-    override fun observeOrderWithProductListByUuid(uuid: String): Flow<List<OrderWithProductEntity>> {
-        return orderEntityQueries.getOrderWithProductByUuid(uuid).asFlow().mapToList()
-    }
-
-    override fun observeLastOrderByUserUuid(userUuid: String): Flow<OrderEntity?> {
-        return orderEntityQueries.getLastOrderWithProductListByUserUuid(userUuid).asFlow()
-            .mapToOneOrNull()
     }
 
     override suspend fun getOrderWithProductListByUuid(uuid: String): List<OrderWithProductEntity> {
