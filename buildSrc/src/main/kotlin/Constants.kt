fbafa8c177c855a64e7da9d02dedfb89d250bbbf
xIdealo <shavl.mark@yandex.ru>
2024-08-01 13:09:31 +0300

    try CI5

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -10,7 +10,7 @@ object Constants {
 }
 
 fun String.getAssembleBundleRelease() =
-    "assemble${this.replaceFirstChar { oldChar -> oldChar.uppercase() }}Release"
+    "assemble${this.replaceFirstChar { oldChar -> oldChar.uppercase() }}"
 
 fun String.getPublishReleaseBundle() =
     "publish${this.replaceFirstChar { oldChar -> oldChar.uppercase() }}ReleaseBundle"
