906a99441fc9238cbb9dd47bd943a0462113c486
xIdealo <shavl.mark@yandex.ru>
2024-08-03 12:40:14 +0300

    try CI11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -236,7 +236,7 @@ tasks.register("assembleAll") {
         PAPA_KARLO_FLAVOR_NAME.getAssembleBundleRelease(),
         YULIAR_FLAVOR_NAME.getAssembleBundleRelease(),
         DJAN_FLAVOR_NAME.getAssembleBundleRelease(),
-        // GUSTO_PUB_FLAVOR_NAME.getAssembleBundleRelease(),
+        GUSTO_PUB_FLAVOR_NAME.getAssembleBundleRelease(),
         TANDIR_HOUSE_FLAVOR_NAME.getAssembleBundleRelease(),
         VKUS_KAVKAZA_FLAVOR_NAME.getAssembleBundleRelease()
     )
