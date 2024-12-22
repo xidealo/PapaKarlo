450709527cbda49215e45a108bd9e23046e89e69
xidealo <shavl.mark@yandex.ru>
2024-12-23 01:40:04 +0300

    update SplashScreen.swift

--- iosApp/iosApp/feature/consumer_cart/ConsumerCartView.swift
+++ iosApp/iosApp/feature/consumer_cart/ConsumerCartView.swift
@@ -32,7 +32,7 @@ struct ConsumerCartView: View {
     
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
-    //State
+    //Navigation
     @State var openCreateOrder: Bool = false
     @State var openLogin: Bool = false
     @State var openProductDetails: Bool = false
@@ -150,7 +150,7 @@ struct ConsumerCartView: View {
         )
     }
 
-    func subscribe(){
+    func subscribe() {
         viewModel.onAction(action: ConsumerCartActionInit())
         listener = viewModel.dataState.watch { consumerCartStateVM in
             if let consumerCartStateVM =  consumerCartStateVM {
@@ -227,7 +227,7 @@ struct ConsumerCartView: View {
         )
     }
 
-    func eventsSubscribe(){
+    func eventsSubscribe() {
         eventsListener = viewModel.events.watch(block: { _events in
             if let events = _events{
                 let consumerCartEvents = events as? [ConsumerCartEvent] ?? []
