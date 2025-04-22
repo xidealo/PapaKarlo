9713128273b89e53da1f283f5c476b5af9f65337
Mark Shavlovskij <36772847+xidealo@users.noreply.github.com>
2025-04-22 18:56:30 +0300

    Feature/update settings ios (#163)
    * update Settings
    
    * add new button

---
+++ iosApp/iosApp/custom_view/card/TextCardWithDivider.swift
@@ -0,0 +1,36 @@
+//
+//  TextCardWithDivider.swift
+//  iosApp
+//
+//  Created by Марк Шавловский on 14.04.2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextCardWithDivider: View {
+    
+    let placeHolder: String
+    let text: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text(placeHolder)
+                .labelSmall(weight: .medium)
+                .foregroundColor(AppColor.onSurfaceVariant)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Text(text)
+                .bodyMedium()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(AppColor.onSurface)
+                .multilineTextAlignment(.leading)
+            
+            FoodDeliveryDivider()
+                .padding(.top, 8)
+        }.padding(.horizontal, Diems.MEDIUM_PADDING)
+            .padding(.vertical, Diems.SMALL_PADDING)
+            .background(AppColor.surface)
+            .cornerRadius(Diems.MEDIUM_RADIUS)
+    }
+}
