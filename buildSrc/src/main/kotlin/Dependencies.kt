b5ad72d000f06c86b282336c54f544b5728f26ef
xIdealo <shavl.mark@yandex.ru>
2025-05-24 22:14:03 +0300

    update version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,10 +5,10 @@ object Namespace {
 
 object CommonApplication {
     private const val versionMajor = 2
-    private const val versionMinor = 5
-    private const val versionPatch = 1
+    private const val versionMinor = 6
+    private const val versionPatch = 0
 
-    const val versionCode = 251
+    const val versionCode = 260
     const val versionName = "$versionMajor.$versionMinor.$versionPatch"
 
     const val deploymentTarget = "12.0"
