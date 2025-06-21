547e706fc95a279f4127f1ba63f98f22f6d6196b
Ivan1 <zhegalin98@yandex.ru>
2025-06-21 03:48:40 +0300

    fix orders status color text

--- app/src/main/java/com/bunbeauty/papakarlo/feature/order/ui/OrderStatusBar.kt
+++ app/src/main/java/com/bunbeauty/papakarlo/feature/order/ui/OrderStatusBar.kt
@@ -1,11 +1,8 @@
 package com.bunbeauty.papakarlo.feature.order.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,14 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bunbeauty.papakarlo.R
 import com.bunbeauty.papakarlo.common.ui.element.card.FoodDeliveryCard
-import com.bunbeauty.papakarlo.common.ui.icon16
 import com.bunbeauty.papakarlo.common.ui.theme.FoodDeliveryTheme
 import com.bunbeauty.shared.domain.model.order.OrderStatus
 
