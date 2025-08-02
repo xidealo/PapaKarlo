6b2dde3ade443536062ad34e74cae75c042de1df
xIdealo <shavl.mark@yandex.ru>
2025-08-02 13:48:41 +0300

    update light logo

---
+++ app/src/main/java/com/bunbeauty/papakarlo/common/ui/theme/color/LimonadColors.kt
@@ -0,0 +1,29 @@
+package com.bunbeauty.papakarlo.common.ui.theme.color
+
+import com.bunbeauty.papakarlo.common.ui.theme.color.FoodDeliveryColors.Gold100
+import com.bunbeauty.papakarlo.common.ui.theme.color.FoodDeliveryColors.LightBlue
+import com.bunbeauty.papakarlo.common.ui.theme.color.FoodDeliveryColors.Orange50
+import com.bunbeauty.papakarlo.common.ui.theme.color.FoodDeliveryColors.Red400
+import com.bunbeauty.papakarlo.common.ui.theme.color.FoodDeliveryColors.Red600
+
+val LimonadColors = AppColors(
+    mainColors = ColorDefaults.lightMainColors(
+        primary = Red600,
+        surfaceVariant = Orange50,
+        strokeVariant = Gold100
+    ),
+    orderColors = ColorDefaults.orderColors(),
+    statusColors = ColorDefaults.statusColors(),
+    bunBeautyBrandColor = LightBlue,
+    isLight = true
+)
+
+val LimonadDarkColors = AppColors(
+    mainColors = ColorDefaults.darkMainColors(
+        primary = Red400
+    ),
+    orderColors = ColorDefaults.orderColors(),
+    statusColors = ColorDefaults.statusColors(),
+    bunBeautyBrandColor = LightBlue,
+    isLight = false
+)
