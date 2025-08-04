897640b997bca55220f6f26c50fd7587a9ded839
xIdealo <shavl.mark@yandex.ru>
2025-08-04 23:58:02 +0300

    update tests

--- shared/src/commonMain/kotlin/com/bunbeauty/shared/domain/feature/payment/GetSelectedPaymentMethodUseCase.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/domain/feature/payment/GetSelectedPaymentMethodUseCase.kt
@@ -3,7 +3,6 @@ package com.bunbeauty.shared.domain.feature.payment
 import com.bunbeauty.shared.domain.model.payment_method.PaymentMethod
 import com.bunbeauty.shared.domain.model.payment_method.SelectablePaymentMethod
 
-//todo tests
 class GetSelectedPaymentMethodUseCase() {
     operator fun invoke(selectablePaymentMethodList: List<SelectablePaymentMethod>): PaymentMethod? {
         return selectablePaymentMethodList.find { paymentMethod ->
