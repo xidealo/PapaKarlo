685c7ccd9ea3660da0af5832c5028381bf672071
xIdealo <shavl.mark@yandex.ru>
2025-10-11 14:47:18 +0300

    update java version

--- analytic/build.gradle.kts
+++ analytic/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         minSdk = AndroidSdk.min
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
