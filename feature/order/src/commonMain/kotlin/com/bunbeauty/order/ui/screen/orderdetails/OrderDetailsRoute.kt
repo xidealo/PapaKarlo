242b8afa0fe02b20ba58b3293c5574d4dfb4c76b
xIdealo <shavl.mark@yandex.ru>
2026-02-24 22:13:09 +0300

    fix OrderDetailsRoute.kt

--- feature/order/src/commonMain/kotlin/com/bunbeauty/order/ui/screen/orderdetails/OrderDetailsRoute.kt
+++ feature/order/src/commonMain/kotlin/com/bunbeauty/order/ui/screen/orderdetails/OrderDetailsRoute.kt
@@ -156,7 +156,8 @@ private fun OrderDetailsSuccessScreen(state: OrderDetailsViewState) {
                 modifier = Modifier.fillMaxSize(),
                 contentPadding =
                     PaddingValues(
-                        bottom = FoodDeliveryTheme.dimensions.screenContentSpace + LocalBottomBarPadding.current,
+                        bottom = FoodDeliveryTheme.dimensions.screenContentSpace
+                                + LocalBottomBarPadding.current,
                     ),
                 verticalArrangement = spacedBy(8.dp),
             ) {
@@ -312,7 +313,8 @@ private fun BottomAmountBar(orderDetailsViewState: OrderDetailsViewState) {
                 Modifier
                     .fillMaxWidth()
                     .background(FoodDeliveryTheme.colors.mainColors.surface)
-                    .padding(FoodDeliveryTheme.dimensions.mediumSpace),
+                    .padding(FoodDeliveryTheme.dimensions.mediumSpace)
+                    .padding(bottom = LocalBottomBarPadding.current),
         ) {
             orderDetailsViewState.discount?.let { discount ->
                 Row(modifier = Modifier.padding(bottom = 8.dp)) {
@@ -471,12 +473,12 @@ private fun getOrderInfo(): OrderDetailsViewState.OrderInfo =
         deferredTime = "10:30",
         address =
             "" +
-                "ул. Лука" +
-                "2" +
-                "10" +
-                "1" +
-                "3" +
-                "тест",
+                    "ул. Лука" +
+                    "2" +
+                    "10" +
+                    "1" +
+                    "3" +
+                    "тест",
         comment = "давай кушать",
         pickupMethod = "доставка",
         statusName = "Готовится",
