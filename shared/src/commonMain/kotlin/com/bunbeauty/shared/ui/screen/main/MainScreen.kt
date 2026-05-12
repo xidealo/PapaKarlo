57a4c114b8f9d4a314b6c62895aa97893847aecc
xIdealo <shavl.mark@yandex.ru>
2026-05-12 13:56:34 +0300

    check commit

--- shared/src/commonMain/kotlin/com/bunbeauty/shared/ui/screen/main/MainScreen.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/ui/screen/main/MainScreen.kt
@@ -75,7 +75,6 @@ fun MainScreen(
             LocalBottomBarPadding provides localBottomBarPadding,
             SharedTransitionScopeComposition provides this,
         ) {
-
             Scaffold(
                 modifier =
                     Modifier
