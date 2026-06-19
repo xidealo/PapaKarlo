f93847eb4d5320a9b174f051736933000d4f5869
deputat <zhegalinie98@yandex.ru>
2026-06-20 00:07:39 +0300

    fix lint

--- core/src/commonMain/kotlin/com/bunbeauty/core/domain/link/GetLinkUseCase.kt
+++ core/src/commonMain/kotlin/com/bunbeauty/core/domain/link/GetLinkUseCase.kt
@@ -1,8 +1,7 @@
 package com.bunbeauty.core.domain.link
 
-import com.bunbeauty.core.model.link.Link
-import com.bunbeauty.core.model.link.LinkType
 import com.bunbeauty.core.domain.repo.LinkRepo
+import com.bunbeauty.core.model.link.Link
 
 expect class GetLinkUseCase(linkRepo: LinkRepo) {
     suspend operator fun invoke(): Link?
