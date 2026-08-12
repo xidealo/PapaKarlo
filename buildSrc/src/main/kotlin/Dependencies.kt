76f630d6695e448b9180d84b5ddd1daade9b6f2f
xIdealo <shavl.mark@yandex.ru>
2026-08-12 18:57:39 +0300

    fix PlatformModule.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,9 +6,9 @@ object Namespace {
 object CommonApplication {
     private const val versionMajor = 3
     private const val versionMinor = 1
-    private const val versionPatch = 2
+    private const val versionPatch = 3
 
-    const val versionCode = 312
+    const val versionCode = 313
     const val versionName = "$versionMajor.$versionMinor.$versionPatch"
 
     const val deploymentTarget = "15.5"
